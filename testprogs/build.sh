7396d6809f6798255114221e933087be34dde825
Michael Kolupaev <michael.kolupaev@clickhouse.com>
2024-07-15 18:45:13 -0700

    .

--- testprogs/build.sh
+++ testprogs/build.sh
@@ -7,7 +7,7 @@ for name in `ls "$BASEDIR" | grep -P '\.c$'`
 do
     name_without_extension="${name%.*}"
     echo $name_without_extension
-    clang -g -O1 -std=c23 -pthread -o "$BASEDIR/$name_without_extension" "$BASEDIR/$name"
+    clang -g -O1 -std=c23 -pthread -o "$BASEDIR/build/$name_without_extension" "$BASEDIR/$name"
 done
 
 FLAGS="-g -O0 -std=c++20 -Wno-vla-cxx-extension"
@@ -15,13 +15,13 @@ for name in `ls "$BASEDIR" | grep -P '\.cpp$'`
 do
     name_without_extension="${name%.*}"
     echo $name_without_extension
-    clang++ -stdlib=libc++ $FLAGS -o "$BASEDIR/$name_without_extension-libc++" "$BASEDIR/$name"
-    clang++ -stdlib=libstdc++ $FLAGS -o "$BASEDIR/$name_without_extension" "$BASEDIR/$name"
+    clang++ -stdlib=libc++ $FLAGS -o "$BASEDIR/build/$name_without_extension-libc++" "$BASEDIR/$name"
+    clang++ -stdlib=libstdc++ $FLAGS -o "$BASEDIR/build/$name_without_extension" "$BASEDIR/$name"
 done
 
 for name in `ls "$BASEDIR" | grep -P '\.rs$'`
 do
     name_without_extension="${name%.*}"
     echo $name_without_extension
-    rustc --edition=2021 -g -o "$BASEDIR/$name_without_extension" "$BASEDIR/$name"
+    rustc --edition=2021 -g -o "$BASEDIR/build/$name_without_extension" "$BASEDIR/$name"
 done
